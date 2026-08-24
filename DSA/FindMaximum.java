d43337aac5486b95f0d4620d6a72f14b6e826db7
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-24 14:08:03 +0530

    Add FindMaximum class to find max in an array

---
+++ DSA/FindMaximum.java
@@ -0,0 +1,18 @@
+public class FindMaximum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 7, 89, 34};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum element: " + max);
+    }
+}
