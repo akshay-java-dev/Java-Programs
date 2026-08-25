076c029822b0b07c1a3b322fc8e3732047e1c64f
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-25 21:16:45 +0530

    Add SecondLargest class to find second largest number

---
+++ DSA/SecondLargest.java
@@ -0,0 +1,22 @@
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 5, 40, 30};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            } else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+
+        System.out.println("Second largest: " + second);
+    }
+}
