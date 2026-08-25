10daf0548275d562f620bfdcd12ead9dcde0cfd6
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-25 21:25:22 +0530

    Add MoveZeros program to shift zeros in array
    This program moves all zeros in the array to the end while maintaining the order of non-zero elements.

---
+++ DSA/MoveZeros.java
@@ -0,0 +1,28 @@
+public class MoveZeros {
+
+    public static void main(String[] args) {
+
+        int[] arr = {0, 1, 0, 3, 12};
+
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.println("Array after moving zeros:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
