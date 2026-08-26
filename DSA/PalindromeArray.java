96a53a4f65898ed809fcaefe5ef16d59c7018cad
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-26 21:23:04 +0530

    Add PalindromeArray class to check for palindromes

---
+++ DSA/PalindromeArray.java
@@ -0,0 +1,29 @@
+public class PalindromeArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 2, 1};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean palindrome = true;
+
+        while (start < end) {
+
+            if (arr[start] != arr[end]) {
+                palindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (palindrome) {
+            System.out.println("Array is palindrome");
+        } else {
+            System.out.println("Array is not palindrome");
+        }
+    }
+}
