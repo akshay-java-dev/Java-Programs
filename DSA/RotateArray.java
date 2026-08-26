dae8251f2653b30ecd83b8b2cff5c86924a54f56
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-26 21:21:46 +0530

    Add RotateArray class for rotating an array
    Implement array rotation by k positions in RotateArray class.

---
+++ DSA/RotateArray.java
@@ -0,0 +1,25 @@
+public class RotateArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+
+        for (int r = 0; r < k; r++) {
+
+            int last = arr[arr.length - 1];
+
+            for (int i = arr.length - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[0] = last;
+        }
+
+        System.out.println("Rotated array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
