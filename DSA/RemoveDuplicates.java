443c731847a9262a71b50be485f6606ed46b0c1d
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-28 10:33:27 +0530

    Add RemoveDuplicates class to filter array elements

---
+++ DSA/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        System.out.print("Array without duplicates: ");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            boolean duplicate = false;
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[i] == arr[j]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (!duplicate) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
