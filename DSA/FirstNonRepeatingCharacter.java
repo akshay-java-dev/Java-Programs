3cf7667618ca86e6bfebf6d093856be5bbe0f775
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-29 21:17:04 +0530

    Add FirstNonRepeatingCharacter class implementation

---
+++ DSA/FirstNonRepeatingCharacter.java
@@ -0,0 +1,28 @@
+public class FirstNonRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        String str = "swiss";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(
+                    "First non-repeating character: " + str.charAt(i)
+                );
+                return;
+            }
+        }
+
+        System.out.println("No non-repeating character");
+    }
+}
