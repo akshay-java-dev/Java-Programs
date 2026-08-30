291d60dcebfb3d593cce6191fc2786bb67af4a99
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-08-30 19:09:38 +0530

    Add MaxSubarraySum class to calculate maximum sum

---
+++ DSA/MaxSubarraySum.java
@@ -0,0 +1,25 @@
+public class MaxSubarraySum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (currentSum + arr[i] > arr[i]) {
+                currentSum = currentSum + arr[i];
+            } else {
+                currentSum = arr[i];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        System.out.println("Maximum subarray sum: " + maxSum);
+    }
+}
