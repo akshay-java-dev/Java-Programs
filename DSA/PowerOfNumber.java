74302504b0532b451f4992810b46de271cdc7cd8
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-09-02 18:23:51 +0530

    Implement power function in PowerOfNumber class

---
+++ DSA/PowerOfNumber.java
@@ -0,0 +1,21 @@
+public class PowerOfNumber {
+
+    static int power(int base, int exponent) {
+
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result = result * base;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int base = 2;
+        int exponent = 5;
+
+        System.out.println("Result: " + power(base, exponent));
+    }
+}
