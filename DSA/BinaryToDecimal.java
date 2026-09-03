554e162a703427f3f90ba1703441180cfc690c6f
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-09-03 17:31:43 +0530

    Add BinaryToDecimal class to convert binary to decimal

---
+++ DSA/BinaryToDecimal.java
@@ -0,0 +1,21 @@
+public class BinaryToDecimal {
+
+    public static void main(String[] args) {
+
+        int binary = 101101;
+        int decimal = 0;
+        int power = 1;
+
+        while (binary > 0) {
+
+            int digit = binary % 10;
+
+            decimal = decimal + digit * power;
+
+            power = power * 2;
+            binary = binary / 10;
+        }
+
+        System.out.println("Decimal: " + decimal);
+    }
+}
