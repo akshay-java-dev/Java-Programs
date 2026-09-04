a2a31092b66479a41417c1dc80e6f11cc4f3732f
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-09-04 21:23:19 +0530

    Add PerfectNumber class to check for perfect numbers

---
+++ DSA/PerfectNumber.java
@@ -0,0 +1,21 @@
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+
+        int number = 28;
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == number) {
+            System.out.println("Perfect number");
+        } else {
+            System.out.println("Not a perfect number");
+        }
+    }
+}
