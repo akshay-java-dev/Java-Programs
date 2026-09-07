d4a5cbcc8d62686450dd237751af5018d144be8e
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-09-07 22:16:04 +0530

    Add ProductOfArrayExceptSelf implementation
    Implement Product of Array Except Self algorithm to calculate the product of all elements except self for a given array.

---
+++ DSA/ProductOfArrayExceptSelf.java
@@ -0,0 +1,28 @@
+public class ProductOfArrayExceptSelf {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+        int n = arr.length;
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    product = product * arr[j];
+                }
+            }
+
+            result[i] = product;
+        }
+
+        System.out.print("Result: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
