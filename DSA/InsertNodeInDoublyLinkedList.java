cb228f56869c98b80b82a9b4b2f9f5c5777e6cd5
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-09-13 11:26:09 +0530

    Implement InsertNodeInDoublyLinkedList class
    This class implements a doubly linked list and includes a method to insert a new node between two existing nodes.

---
+++ DSA/InsertNodeInDoublyLinkedList.java
@@ -0,0 +1,38 @@
+public class InsertNodeInDoublyLinkedList {
+
+    static class Node {
+        int data;
+        Node previous;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node first = new Node(10);
+        Node second = new Node(30);
+
+        first.next = second;
+        second.previous = first;
+
+        Node newNode = new Node(20);
+
+        newNode.previous = first;
+        newNode.next = second;
+
+        first.next = newNode;
+        second.previous = newNode;
+
+        Node current = first;
+
+        System.out.print("After Insertion: ");
+
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
