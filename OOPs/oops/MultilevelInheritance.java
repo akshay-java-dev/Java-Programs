35c52fd65a4a433baf4bca6d5a4e32411d29d643
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-09-15 09:04:51 +0530

    Add MultilevelInheritance class with Animal and Dog
    Implement multilevel inheritance with Animal and Dog classes.

---
+++ OOPs/oops/MultilevelInheritance.java
@@ -0,0 +1,29 @@
+lass Animal {
+
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class MultilevelInheritance extends Dog {
+
+    void run() {
+        System.out.println("Dog runs");
+    }
+
+    public static void main(String[] args) {
+
+        MultilevelInheritance obj = new MultilevelInheritance();
+
+        obj.eat();
+        obj.bark();
+        obj.run();
+    }
+}
