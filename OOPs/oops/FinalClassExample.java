54b5a2939c2a81b9d91013b6925b46b9a7497c7d
Akshay Kumbhar <kumbharakshay471@gmail.com>
2026-09-25 10:34:13 +0530

    Add final class Vehicle with start method

---
+++ OOPs/oops/FinalClassExample.java
@@ -0,0 +1,16 @@
+final class Vehicle {
+
+    void start() {
+        System.out.println("Vehicle starts");
+    }
+}
+
+public class FinalClassExample {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+
+        vehicle.start();
+    }
+}
